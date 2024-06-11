97627acc920f18fd7a8ecf28c56fefd18c15da49
AlessoGozzo <alego91@liu.se>
2024-06-11 12:26:52 +0200

    poop

---
+++ Git.R
@@ -0,0 +1,21 @@
+usethis::use_git()
+
+usethis::git_sitrep()
+
+usethis::use_git_config(user.name = "AlessoGozzo", user.email = "alego91@liu.se")
+
+#Takes you to github.com create new token.
+usethis::create_github_token()
+
+#Use to set your new token in your configuration
+gitcreds::gitcreds_set()
+
+
+
+usethis::use_git()
+usethis::use_github()
+  
+
+poem <- print("Lovliest of trees the cherry now")
+
+getwd()
